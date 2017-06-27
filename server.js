6e8ec4c369787408dd9e977ea96d4198890d73a7
Peter Makadi <petermakadi@Peters-MacBook-Pro.local>
2017-06-27 16:50:33 +0300

    server answering to ajax json requests

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost/footyDB', function() {
-//   console.log("DB connection succesfully established!!!");
-// })
+mongoose.connect('mongodb://localhost/footyDB', function() {
+  console.log("DB connection succesfully established!!!");
+})
 
 var app = express();
 
@@ -14,7 +14,18 @@ app.use(express.static('database'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var handler = function(res) {
+  return function(err, data) {
+    if (err) {
+      throw err;
+    }
+    res.send(data);
+  }
+};
 
+app.get('/epl', function(req, res) {
+  Post.find(handler(res));
+});
 
 app.listen(3000, function() {
   console.log("Server is running, lets kick some balls.");
