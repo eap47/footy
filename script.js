da069e14d7d8a516b8d3312e80fd1299d99b64ea
raphaeldanan <raphidanan123@gmail.com>
2017-06-27 17:47:22 +0300

    hvhv

---
+++ script.js
@@ -0,0 +1,80 @@
+
+var selectedFixtures=[];
+
+
+function fetchFixtures () {
+
+  for (var i=0;i<fixtures.length;i++) {
+      selectedFixtures.empty();
+    	if ( (fixtures[i].HomeTeam === teamA || fixtures[i].HomeTeam === teamB)  && (fixtures[i].AwayTeam === teamA || fixtures[i].AwayTeam === teamB) ) {
+  		selectedFixtures.push(fixtures[i]);
+    	}
+      addResult();
+};
+
+function addResult() {
+    for (var i=0;i<selectedFixtures.length;i++){
+        var sf = selectedFixtures[i]
+        if (sf.FTHG > sf.FTAG) {
+            sf.result=sf.HomeTeam
+        } else if (sf.FTHG < sf.FTAG) {
+            sf.result=sf.AwayTeam
+        } else {
+            sf.result="draw"
+        }
+    }
+};
+
+function pieChart (result) {
+    var teamAhome = {
+        teamAwins: 0,
+        draws: 0,
+        teamBwins: 0
+    };
+    var teamBhome = {
+        teamBwins: 0,
+        draws: 0,
+        teamAwins: 0
+    };
+    var total = {
+        teamA: teamAhome.teamAwins + teamBhome.teamAwins,
+        draws: teamAhome.draws + teamBhome.draws,
+        teamB: teamBhome.teamBwins + teamAhome.teamBwins
+    };
+
+    for (var i=0;i<selectedFixtures.length;i++) {
+        var sf = selectedFixtures;
+        if(sf.HomeTeam === teamA && result===teamA){
+            teamAhome.teamAwins++;
+        } else if (sf.Hometeam === teamA && result === teamB) {
+            teamAhome.teamBwins++;
+        } else {
+            teamAhome.draws++;
+        }
+    }
+
+}
+    for (var i=0;i<selectedFixtures.length;i++) {
+        var sf = selectedFixtures;
+        if(sf.AwayTeam === teamA && result===teamA){
+            teamAaway.teamAwins++;
+        } else if (sf.Awayteam === teamA && result === teamB) {
+            teamAaway.teamBwins++;
+        } else {
+            teamAaway.draws++;
+        }
+    }
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
